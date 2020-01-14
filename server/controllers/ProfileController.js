f407d61408b9fb09f4ef32f1345bb695c9f95688
Jim Greaves <greavesjim3@gmail.com>
2020-01-14 13:47:55 -0700

    added profile routes

---
+++ server/controllers/ProfileController.js
@@ -0,0 +1,43 @@
+import _profileService from "../services/ProfileService";
+import express from "express";
+import { Authorize } from "../middleware/authorize.js";
+
+//PUBLIC
+export default class ProfilesController {
+  constructor() {
+    this.router = express
+      .Router()
+      .get("/:id", this.getById)
+      .use(Authorize.authenticated)
+      .put("/:id", this.edit)
+      .use(this.defaultRoute);
+  }
+
+  // this is pretty neat
+
+  defaultRoute(req, res, next) {
+    next({ status: 404, message: "No Such Route" });
+  }
+
+  async getById(req, res, next) {
+    try {
+      let data = await _profileService.getById(req.params.id);
+      return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      let data = await _profileService.edit(
+        req.params.id,
+        req.session.uid,
+        req.body
+      );
+      return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
