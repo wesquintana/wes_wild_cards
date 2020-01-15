5cb4097ff2e14ebf8b2d6e2a633df77e506d9220
wesquintana <wesquintana@gmail.com>
2020-01-15 15:28:21 -0700

    added sweetalerts to login

--- client/src/main.js
+++ client/src/main.js
@@ -1,19 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/index'
-import store from './store/index'
-import AuthService from "./AuthService"
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router/index";
+import store from "./store/index";
+import AuthService from "./AuthService";
 
 //Vue.config.productionTip = false
 
 async function init() {
-  let user = await AuthService.Authenticate()
-  if (user) { store.commit("setUser", user) }
-  else { router.push({ name: 'login' }) }
+  let user = await AuthService.Authenticate();
+  if (user) {
+    store.commit("setUser", user);
+  }
+  // else { router.push({ name: 'login' }) }
   new Vue({
     router,
     store,
     render: h => h(App)
-  }).$mount('#app')
+  }).$mount("#app");
 }
-init()
+init();
