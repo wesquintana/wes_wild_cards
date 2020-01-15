bfcf5410282495c067a50f34cfa488767457c717
laceyeric <elaceyinfo@yahoo.com>
2020-01-15 11:56:57 -0700

    creates DeckModule

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,10 +3,15 @@
     <div class="row">
       <div class="col-12">
         <h2>My Decks</h2>
-        <div class="row"></div>
+        <div class="row">
+          <div class="col-3" v-for="userDeck in userDecks" :key="userDeck._id">{{ userDeck }}</div>
+        </div>
       </div>
       <div class="col-12">
         <h2>Public Decks</h2>
+        <div class="row">
+          <div class="col-3" v-for="publicDeck in publicDecks" :key="publicDeck._id">{{ userDeck }}</div>
+        </div>
       </div>
     </div>
   </div>
@@ -15,8 +20,19 @@
 <script>
 export default {
   name: "home",
-  mounted() {},
-  data() {}
+  mounted() {
+    this.$store.dispatch("getUserDecks");
+    this.$store.dispatch("getPublicDecks");
+  },
+  data() {},
+  computed: {
+    userDecks() {
+      return this.$store.state.userDecks;
+    },
+    publicDecks() {
+      return this.$store.state.publicDecks;
+    }
+  }
 };
 </script>
 
