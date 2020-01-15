f13d1f0800cde5d1b9bab3f5aa7e509c2812cd96
laceyeric <elaceyinfo@yahoo.com>
2020-01-15 16:09:00 -0700

    changes Swal to Bootstrap modal

--- client/src/components/NewDeck.vue
+++ client/src/components/NewDeck.vue
@@ -1,10 +1,40 @@
 <template>
   <div class="new-deck">
-    <div class="card" style="width: 10rem; height: 14rem;">
+    <div
+      class="card"
+      style="width: 10rem; height: 14rem;"
+      data-toggle="modal"
+      data-target="#newDeckModal"
+    >
+      <div
+        class="modal fade"
+        id="newDeckModal"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="newDeckModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="newDeckModalLabel">Create a Deck</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">...</div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="card-body">
         <h5 class="card-title mt-2">New Deck</h5>
-        <div class="d-flex justify-content-center align-items-center"></div>
-        <i class="fas fa-plus" id="add-deck"></i>
+        <div class="d-flex justify-content-center align-items-center">
+          <i class="fas fa-plus" id="add-deck"></i>
+        </div>
       </div>
     </div>
   </div>
@@ -13,9 +43,15 @@
 <script>
 export default {
   name: "DeckSticker",
-  mounted() {}
+  mounted() {},
+  methods: {
+    newDeck() {}
+  }
 };
 </script>
 
 <style>
+.card {
+  cursor: pointer;
+}
 </style>
