ed15fa495191de7200a12f3d5a48a7b2378de91b
laceyeric <elaceyinfo@yahoo.com>
2020-01-15 14:03:28 -0700

    setting up decks on home view

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,13 +3,21 @@
     <div class="row">
       <div class="col-12">
         <h2>My Decks</h2>
-        <div class="row scroll-x justify-content-between">
-          <div class="col-3" v-for="userDeck in userDecks" :key="userDeck._id">
-            <div class="card" style="width: 18rem;">
+        <div class="row scroll-x">
+          <div class="col-2">
+            <div class="card" style="width: 10rem; height: 14rem;">
+              <div class="card-body">
+                <h5 class="card-title mt-2">New Deck</h5>
+                <div class="d-flex justify-content-center align-items-center"></div>
+                <i class="fas fa-plus" id="add-deck"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-2" v-for="userDeck in userDecks" :key="userDeck._id">
+            <div class="card" style="width: 10rem; height: 14rem;">
               <div class="card-body">
                 <h5 class="card-title">{{ userDeck.title }}</h5>
                 <p class="card-text">{{ userDeck.description }}</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
               </div>
             </div>
           </div>
@@ -18,12 +26,11 @@
       <div class="col-12">
         <h2>Public Decks</h2>
         <div class="row">
-          <div class="col-3" v-for="publicDeck in publicDecks" :key="publicDeck._id">
-            <div class="card" style="width: 18rem;">
+          <div class="col-2" v-for="publicDeck in publicDecks" :key="publicDeck._id">
+            <div class="card" style="width: 10rem; height: 14rem;">
               <div class="card-body">
                 <h5 class="card-title">{{ publicDeck.title }}</h5>
                 <p class="card-text">{{ publicDeck.description }}</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
               </div>
             </div>
           </div>
@@ -72,5 +79,9 @@ scroll-x {
 
 .scroll-x::-webkit-scrollbar-thumb:hover {
   background-color: rgba(100, 100, 100, 0.9);
+}
+
+#add-deck {
+  font-size: 6rem;
 }
 </style>
