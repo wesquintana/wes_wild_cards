fc4fc8af575a5b25acd3f0cc88f593b9ef3151b6
laceyeric <elaceyinfo@yahoo.com>
2020-01-17 13:46:27 -0700

    card detail modal framework filled in

--- client/src/components/CardDetails.vue
+++ client/src/components/CardDetails.vue
@@ -1,84 +1,114 @@
 <template>
-  <div class="card-details">
+  <div class="container-fluid card-details">
     <div
-      class="modal fade"
+      class="modal fade row"
       data-backdrop
       id="cardDetailsModal"
       tabindex="-1"
       role="dialog"
-      aria-labelledby="newCardModalLabel"
+      aria-labelledby="cardDetailsModalLabel"
       aria-hidden="true"
     >
       <div class="modal-dialog" role="document">
         <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="newCardModalLabel">Create a Card</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <div class="modal-header text-center">
+            <h5 class="modal-title" id="activeCardModalLabel">{{ activeCard.name }}</h5>
+            <!--TODO create clear Active Card method -->
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+              @click="clearActiveCard"
+            >
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <!-- Form code -->
-          <form @submit.prevent="addCard">
-            <div class="modal-body">
-              <div class="form-group text-left">
-                <label for="newCardName">Card Name</label>
-                <input
-                  v-model="newCard.name"
-                  type="text"
-                  class="form-control"
-                  id="newCardTitle"
-                  placeholder="Name your card"
-                  required
-                />
+          <div class="col">
+            <!-- card code -->
+            <div class="card mb-4" style="width: 20rem; height: 28rem;">
+              <div class="card-body justify-content-between">
+                <h5 class="card-title text-wrap">{{ activeCard.name }}</h5>
+                <h5 class="card-title text-wrap">{{ activeCard.category }}</h5>
               </div>
-              <div class="form-group text-left">
-                <label for="newCardCategory">Card Category</label>
-                <input
-                  v-model="newCard.category"
-                  type="text"
-                  class="form-control"
-                  id="newCardCategory"
-                  placeholder="Category..."
-                />
+              <div class="card-body">
+                <!-- card face image (top half of card) -->
+                <img :src="activeCard.imgFace" placeholder />
               </div>
-              <div class="form-group text-left">
-                <label for="newCardContent">Card Content</label>
-                <textarea
-                  v-model="newCard.content"
-                  class="form-control"
-                  id="newCardContent"
-                  placeholder="Share some details about your Card..."
-                  required
-                ></textarea>
-              </div>
-              <div class="form-group text-left">
-                <input
-                  v-model="newCard.imgFace"
-                  type="text"
-                  class="form-control"
-                  id="cardImgFace"
-                  placeholder="Place Card Face Image URL Here..."
-                />
-                <label class="form-group-label" for="cardImgFace">Choose file, optional</label>
-              </div>
-              <div class="form-group text-left">
-                <input
-                  v-model="newCard.imgBack"
-                  type="text"
-                  class="form-control"
-                  id="cardImgBack"
-                  placeholder="Place Card Back Image URL Here..."
-                />
-                <label class="form-group-label" for="cardImgBack">Choose file, optional</label>
+              <div class="card-body">
+                <p class="card-text text-left text-wrap">{{ activeCard.content }}</p>
               </div>
             </div>
-            <div class="modal-footer d-flex justify-content-center">
-              <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
-              <button type="submit" class="btn btn-primary d-flex justify-content-left">Create card!</button>
-            </div>
-          </form>
-          <!-- end Form code -->
+          </div>
+          <!-- column of buttons -->
+          <div class="col-3">
+            <button>Flip Card</button>
+            <button>Copy Card</button>
+            <button>Edit Card</button>
+            <button>Delete Card</button>
+          </div>
         </div>
+        <!-- form code -->
+        <form @submit.prevent="editCard">
+          <div class="modal-body">
+            <div class="form-group text-left">
+              <label for="activeCardName">Card Name</label>
+              <input
+                v-model="activeCard.name"
+                type="text"
+                class="form-control"
+                id="activeCardTitle"
+                placeholder="Name your card"
+                required
+              />
+            </div>
+            <div class="form-group text-left">
+              <label for="activeCardCategory">Card Category</label>
+              <input
+                v-model="activeCard.category"
+                type="text"
+                class="form-control"
+                id="activeCardCategory"
+                placeholder="Category..."
+              />
+            </div>
+            <div class="form-group text-left">
+              <label for="activeCardContent">Card Content</label>
+              <textarea
+                v-model="activeCard.content"
+                class="form-control"
+                id="activeCardContent"
+                placeholder="Share some details about your Card..."
+                required
+              ></textarea>
+            </div>
+            <div class="form-group text-left">
+              <input
+                v-model="activeCard.imgFace"
+                type="text"
+                class="form-control"
+                id="cardImgFace"
+                placeholder="Place Card Face Image URL Here..."
+              />
+              <label class="form-group-label" for="cardImgFace">Choose file, optional</label>
+            </div>
+            <div class="form-group text-left">
+              <input
+                v-model="activeCard.imgBack"
+                type="text"
+                class="form-control"
+                id="cardImgBack"
+                placeholder="Place Card Back Image URL Here..."
+              />
+              <label class="form-group-label" for="cardImgBack">Choose file, optional</label>
+            </div>
+          </div>
+          <div class="modal-footer d-flex justify-content-center">
+            <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
+            <button type="submit" class="btn btn-primary d-flex justify-content-left">Save changes!</button>
+          </div>
+        </form>
+        <!-- end Form code -->
       </div>
     </div>
   </div>
