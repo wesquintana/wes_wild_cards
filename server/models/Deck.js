b99cca160fa727ad9a562b5d92fcb08b90e04f15
Jim Greaves <greavesjim3@gmail.com>
2020-01-31 09:27:29 -0700

    deckback changes

--- server/models/Deck.js
+++ server/models/Deck.js
@@ -1,6 +1,7 @@
-import mongoose from "mongoose"
-let Schema = mongoose.Schema
-let ObjectId = Schema.Types.ObjectId
+import mongoose from "mongoose";
+
+let Schema = mongoose.Schema;
+let ObjectId = Schema.Types.ObjectId;
 
 // NOTE adding Card as a sub doc of Deck
 const Card = new Schema({
@@ -8,19 +9,25 @@ const Card = new Schema({
   content: { type: String, required: true },
   authorId: { type: ObjectId, ref: "User", required: true },
   imgFace: { type: String, required: false },
-  imgBack: { type: String, required: false },
+  imgBack: {
+    type: String,
+    default: "/square_back_of_card.jpg"
+  },
   category: { type: String, required: false }
 });
 // NOTE Card has to be the end of the inheritance chain and is dependent upon the parent (cannot be retrieved on its own without parent)
 
-const Deck = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  rules: { type: String, required: true },
-  authorId: { type: ObjectId, ref: 'User', required: true },
-  isPrivate: { type: Boolean, required: true },
-  cards: [Card],
-  img: { type: String, required: false }
-}, { timestamps: true })
+const Deck = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    rules: { type: String, required: true },
+    authorId: { type: ObjectId, ref: "User", required: true },
+    isPrivate: { type: Boolean, required: true },
+    cards: [Card],
+    img: { type: String, required: false }
+  },
+  { timestamps: true }
+);
 
-export default Deck
+export default Deck;
