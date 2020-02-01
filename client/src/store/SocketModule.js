d57ce90310b2c62870ef93fceb53866562d2d703
wesquintana <wesquintana@gmail.com>
2020-01-31 21:50:34 -0700

    adds rooms

--- client/src/store/SocketModule.js
+++ client/src/store/SocketModule.js
@@ -4,17 +4,19 @@ let socket = {};
 
 export default {
   actions: {
-    initializeSocket({ commit, dispatch }) {
+    initializeSocket({ commit, dispatch }, lobbyId) {
       //establish connection with socket
       socket = io("//localhost:3000");
       //Handle any on connection events
-      socket.on("CONNECTED", data => {
-        console.log("Connected to socket");
+      socket.on("connect", () => {
+        console.log(lobbyId);
+        socket.emit('join', lobbyId);
       });
 
       // register all listeners\
       // NOTE do we need this?
       socket.on("moveCard", data => {
+        console.log("nothing gets here")
         commit("moveCard", data);
       });
       socket.on("shuffle", data => {
