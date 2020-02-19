711a43daa4881dc5f776ea5ef50e07fb6ea8f6fc
wesquintana <wesquintana@gmail.com>
2020-02-19 12:02:24 -0700

    hopefully allows for foreign requests

--- server/main.js
+++ server/main.js
@@ -19,7 +19,11 @@ Socket.setIO(io);
 var port = process.env.PORT || 3000;
 server.use(express.static(__dirname + "/../client/dist"));
 
-var whitelist = ["http://localhost:8080", "http://localhost:8081"];
+var whitelist = [
+  "http://localhost:8080",
+  "http://localhost:8081",
+  "https://wild-cards.herokuapp.com"
+];
 var corsOptions = {
   origin: function(origin, callback) {
     var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
